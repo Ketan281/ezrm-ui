95f4781a7629952f6fd22bc39e86026a2786771b
Ketan281 <ketanmohite8307@gmail.com>
2025-05-14 14:55:50 +0530

    saved change

--- src/app/admin/inventory/update/Detail.tsx
+++ src/app/admin/inventory/update/Detail.tsx
@@ -68,7 +68,6 @@ const theme = createTheme({
         MuiCssBaseline: {
             styleOverrides: {
                 '@global': {
-                    '@import': `url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap')`,
                     '*': {
                         fontFamily: '"Poppins", sans-serif',
                     },
