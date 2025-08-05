9b21f22476226e6646203dbc9481a6d2b3a317a9
Ketan281 <ketanmohite8307@gmail.com>
2025-08-05 12:40:40 +0530

    saved changes

--- src/api/config/endpoints.ts
+++ src/api/config/endpoints.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5007/api/v1"
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL|| "http://localhost:5007/api/v1"
 
 export const ENDPOINTS = {
   BASE_URL,
